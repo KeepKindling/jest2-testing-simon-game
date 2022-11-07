f5ba35918eb4ca7a329c6daba202b886782885b6
KeepKindling <flashywizard127@gmail.com>
2022-11-07 11:22:42 +0000

    add incremental tests and code for addTurn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game")
+const { game, newGame, showScore, addTurn } = require("../game")
 
 beforeAll(() => {
     let fs = require("fs");
@@ -33,21 +33,21 @@ describe("game objects contains correct keys", () => {
 describe("newGame works correctly", () => {
     beforeAll(() => {
         game.score = 42;
-        game.currentGame = ["button1", "button2"];
         game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
         document.getElementById("score").innerText = "42";
         newGame();
     });
-    test("should set game score to 0", () => {
+    test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear the computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
-    });
-    test("should clear the playerMoves array", () => {
-        expect(game.playerMoves.length).toBe(0)
-    });
-    test("should display 0 for the element with the id of score", () => {
+    test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
 });
